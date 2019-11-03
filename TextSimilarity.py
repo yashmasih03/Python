bcf3ec445a0e754351068a26c6c4201eb491c721
randerson112358 <randerson112358@users.noreply.github.com>
2019-11-02 23:20:23 -0400

    Update TextSimilarity.py

--- TextSimilarity.py
+++ TextSimilarity.py
@@ -1,7 +1,10 @@
+#import the library sklearn to use the cosine similarity function
 from sklearn.metrics.pairwise import cosine_similarity
 
+#A list of text
 text = ["Amazing Spiderman Amazing","Spiderman Spiderman Amazing"]
 
+#Create a Count Vector object to get a count of each word in the text
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer()
 count_matrix = cv.fit_transform(text)
