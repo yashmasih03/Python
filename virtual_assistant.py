69a1d1a85afe40b44f259385d16c7e1534b646c0
randerson112358 <randerson112358@users.noreply.github.com>
2019-12-11 23:58:01 -0500

    Update virtual_assistant.py

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -1,5 +1,5 @@
-# Description: This is a virtual assistant program that gets the date, responds back with a random greeting, and returns a response
-#             getting information on a person from wikipedia e.g. 'who is LeBron James'
+# Description: This is a virtual assistant program that gets the date, the current time, responds back with a random greeting, and 
+#             returns information on a person from wikipedia e.g. 'who is LeBron James'
 
 # A virtual assistant is an application that can understand voice commands and complete tasks for a user.
 
